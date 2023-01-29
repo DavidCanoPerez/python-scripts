66a8d353ff0fe78cbdce90b8107900dbdfc541fa
David Cano <davidcanoperez5504@gmail.com>
2023-01-29 23:35:14 +0100

    Create 00.basicos.depuracion.01.py

---
+++ 00.basicos.depuracion.01/00.basicos.depuracion.01.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+    ejemplo depuracion
+        utilizando la libreria pdb
+    
+    documentacion
+        https://docs.python.org/es/3/library/pdb.html
+    
+    comprobar comandos
+        v
+            para ver valor de variable
+        n
+            (next) continua ejecucion de programa
+        q 
+            finaliza depuracion
+"""
+
+import pdb
+
+v = 0
+while v < 5:
+    pdb.set_trace()     # punto de interrupcion
+    v += 1
+print("fin")
