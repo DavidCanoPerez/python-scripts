038ab32ac2a1b97416d1b20591c33795ca627ba0
David Cano <davidcanoperez5504@gmail.com>
2023-02-03 04:37:19 +0100

    qtdesigner.plantilla

---
+++ qt.designer.pyqt5.01.plantilla/MainWindow.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5 import uic
+
+
+from PyQt5.QtMultimedia import *
+from PyQt5.QtMultimediaWidgets import *
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self, parent)
+        uic.loadUi("MainWindow.ui", self)
+
--- qt.designer.pyqt5.01.plantilla/main.py
+++ qt.designer.pyqt5.01.plantilla/main.py
@@ -5,22 +5,12 @@
 """
 
 import sys
-
-from ejemplo import Ui_MainWindow
-#from ejemplo import *
-
-from PyQt5.QtWidgets import ( QApplication, QMainWindow 
-                             )
-
-class Window(QMainWindow, Ui_MainWindow):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-
+from PyQt5.QtWidgets import QApplication 
+from MainWindow import MainWindow 
+                             
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    win = Window()
+    app = QApplication([])
+    win = MainWindow()
     win.show()
     sys.exit(app.exec())
         
