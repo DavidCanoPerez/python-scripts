a1fd8b254e21b6a29e3e47dd148cfae566c63066
David Cano <davidcanoperez5504@gmail.com>
2023-02-23 09:33:01 +0100

    Create 00.funciones.integradas.py

---
+++ 00.funciones.integradas/00.funciones.integradas.py
@@ -0,0 +1,430 @@
+# -*- coding: utf-8 -*-
+
+"""
+    funciones integradas (built-in functions)
+    están disponibles sin necesidad de importar ninguna biblioteca
+"""
+
+"""
+    __import__()
+    https://docs.python.org/3/library/functions.html#import__
+"""
+
+
+"""
+    abs()
+    https://docs.python.org/3/library/functions.html#abs
+"""
+
+
+"""
+    aiter()
+    https://docs.python.org/3/library/functions.html#aiter
+"""
+
+"""
+    all()
+    https://docs.python.org/3/library/functions.html#all
+"""
+
+
+"""
+    any()
+    https://docs.python.org/3/library/functions.html#any
+"""
+
+
+"""
+    anext()
+    https://docs.python.org/3/library/functions.html#anext
+"""
+
+"""
+    ascii()
+    https://docs.python.org/3/library/functions.html#ascii
+"""
+
+
+"""
+    bin()
+    https://docs.python.org/3/library/functions.html#bin
+"""
+
+
+"""
+    bool()
+    https://docs.python.org/3/library/functions.html#bool
+"""
+
+"""
+    breakpoint()
+    https://docs.python.org/3/library/functions.html#breakpoint
+"""
+
+
+"""
+    bytearray()
+    https://docs.python.org/3/library/functions.html#func-bytearray
+"""
+
+
+"""
+    bytes()
+    https://docs.python.org/3/library/functions.html#func-bytes
+"""
+
+
+"""
+    callable()
+    https://docs.python.org/3/library/functions.html#callable
+"""
+
+"""
+    chr()
+    https://docs.python.org/3/library/functions.html#chr
+"""
+
+
+"""
+    classmethod()
+    https://docs.python.org/3/library/functions.html#classmethod
+"""
+
+
+"""
+    compile()
+    https://docs.python.org/3/library/functions.html#compile
+"""
+
+
+"""
+    complex()
+    https://docs.python.org/3/library/functions.html#complex
+"""
+
+
+"""
+    delattr()
+    https://docs.python.org/3/library/functions.html#delattr
+"""
+
+
+"""
+    dict()
+    https://docs.python.org/3/library/functions.html#func-dict
+"""
+
+
+"""
+    dir()
+    https://docs.python.org/3/library/functions.html#dir
+"""
+
+
+"""
+    divmod()
+    https://docs.python.org/3/library/functions.html#divmod
+"""
+
+
+"""
+    enumerate()
+    https://docs.python.org/3/library/functions.html#enumerate
+"""
+
+"""
+    eval()
+    https://docs.python.org/3/library/functions.html#eval
+"""
+
+
+"""
+    exec()
+    https://docs.python.org/3/library/functions.html#exec
+"""
+
+
+"""
+    filter()
+    https://docs.python.org/3/library/functions.html#filter
+"""
+
+
+"""
+    float()
+    https://docs.python.org/3/library/functions.html#float
+"""
+
+
+"""
+    format()
+    https://docs.python.org/3/library/functions.html#format
+"""
+
+
+"""
+    frozenset()
+    https://docs.python.org/3/library/functions.html#func-frozenset
+"""
+
+
+"""
+    getattr()
+    https://docs.python.org/3/library/functions.html#getattr
+"""
+
+
+"""
+    globals()
+    https://docs.python.org/3/library/functions.html#globals
+"""
+
+
+"""
+    hasattr()
+    https://docs.python.org/3/library/functions.html#hasattr
+"""
+
+
+"""
+    hash()
+    https://docs.python.org/3/library/functions.html#hash
+"""
+
+
+"""
+    help()
+    https://docs.python.org/3/library/functions.html#help
+"""
+
+
+"""
+    hex()
+    https://docs.python.org/3/library/functions.html#hex
+"""
+
+
+"""
+    id()
+    https://docs.python.org/3/library/functions.html#id
+"""
+
+
+"""
+    input()
+    https://docs.python.org/3/library/functions.html#input
+"""
+
+
+"""
+    int()
+    https://docs.python.org/3/library/functions.html#int
+"""
+
+
+"""
+    isinstance()
+    https://docs.python.org/3/library/functions.html#isinstance
+"""
+
+
+"""
+    issubclass()
+    https://docs.python.org/3/library/functions.html#issubclass
+"""
+
+
+"""
+    iter()
+    https://docs.python.org/3/library/functions.html#iter
+"""
+
+
+"""
+    len()
+    https://docs.python.org/3/library/functions.html#len
+"""
+
+
+"""
+    list()
+    https://docs.python.org/3/library/functions.html#func-list
+"""
+
+
+"""
+    locals()
+    https://docs.python.org/3/library/functions.html#locals
+"""
+
+
+"""
+    map()
+    https://docs.python.org/3/library/functions.html#map
+"""
+
+
+"""
+    max()
+    https://docs.python.org/3/library/functions.html#max
+"""
+
+
+"""
+    memoryview()
+    https://docs.python.org/3/library/functions.html#func-memoryview
+"""
+
+
+"""
+    min()
+    https://docs.python.org/3/library/functions.html#min
+"""
+
+
+"""
+    next()
+    https://docs.python.org/3/library/functions.html#next
+"""
+
+
+"""
+    object()
+    https://docs.python.org/3/library/functions.html#object
+"""
+
+
+"""
+    oct()
+    https://docs.python.org/3/library/functions.html#oct
+"""
+
+
+"""
+    open()
+    https://docs.python.org/3/library/functions.html#open
+"""
+
+
+"""
+    ord()
+    https://docs.python.org/3/library/functions.html#ord
+"""
+
+
+"""
+    pow()
+    https://docs.python.org/3/library/functions.html#pow
+"""
+
+
+"""
+    print()
+    https://docs.python.org/3/library/functions.html#print
+"""
+
+
+"""
+    property()
+    https://docs.python.org/3/library/functions.html#property
+"""
+
+"""
+    range()
+    https://docs.python.org/3/library/functions.html#func-range
+"""
+
+
+"""
+    repr()
+    https://docs.python.org/3/library/functions.html#repr
+"""
+
+
+"""
+    reversed()
+    https://docs.python.org/3/library/functions.html#reversed
+"""
+
+
+"""
+    round()
+    https://docs.python.org/3/library/functions.html#round
+"""
+
+
+"""
+    set()
+    https://docs.python.org/3/library/functions.html#func-set
+"""
+
+
+"""
+    setattr()
+    https://docs.python.org/3/library/functions.html#setattr
+"""
+
+
+"""
+    slice()
+    https://docs.python.org/3/library/functions.html#slice
+"""
+
+
+"""
+    sorted()
+    https://docs.python.org/3/library/functions.html#sorted
+"""
+
+
+"""
+    staticmethod()
+    https://docs.python.org/3/library/functions.html#staticmethod
+"""
+
+
+"""
+    str()
+    https://docs.python.org/3/library/functions.html#func-str
+"""
+
+
+"""
+    sum()
+    https://docs.python.org/3/library/functions.html#sum
+"""
+
+
+"""
+    super()
+    https://docs.python.org/3/library/functions.html#super
+"""
+
+
+"""
+    tuple()
+    https://docs.python.org/3/library/functions.html#func-tuple
+"""
+
+
+"""
+    type()
+    https://docs.python.org/3/library/functions.html#type
+"""
+
+
+"""
+    vars()
+    https://docs.python.org/3/library/functions.html#vars
+"""
+
+
+"""
+    zip()
+    https://docs.python.org/3/library/functions.html#zip
+"""
+
+
+
+
+
