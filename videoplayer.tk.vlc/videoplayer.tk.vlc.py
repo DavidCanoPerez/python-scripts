10a4a34160900b27bc733e87eee5fd065d0296e6
David Cano <davidcanoperez5504@gmail.com>
2023-11-09 00:57:16 +0100

    Create videoplayer.tk.vlc.py

---
+++ videoplayer.tk.vlc/videoplayer.tk.vlc.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+    reproductor de video utilizando tk y vlc
+    
+    pip install python-vlc
+    
+    scr en:
+    https://git.videolan.org/?p=vlc/bindings/python.git;a=commit;h=e8d4373a7e9699f0a5a533cb0da144a037a08c2a
+    
+    también otras versiones en:
+    https://github.com/oaubert/python-vlc/blob/master/examples/tkvlc.py
+"""
