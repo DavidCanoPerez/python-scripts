8ac68429d110b3382f328dfe389bd952c8209a25
David Cano <davidcanoperez5504@gmail.com>
2023-11-11 20:51:01 +0100

    updated

--- 00.tipos.datos/00.tipos.datos.py
+++ 00.tipos.datos/00.tipos.datos.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 
+"""
+    https://blog.hubspot.es/website/lista-python
+"""
 
 #   tipos de datos:
 #       cadenas - strings
 #               en python no hay variables tipo char o de un único caracter
+#               cadena = "sistema"
+#               caracter = "/"
 #       números
 #               en python los booleanos (bool) son una subclase de los enteros
+#               x = 2
+#               b = True
 #       conjuntos - sets
 #               conjunto = {'a', 'b', 'c'}
 #       lista - list
@@ -20,9 +27,9 @@
 #       mientras que las tuplas trabajan más rápido, pero no pueden ser modificadas.
 
 
-# ----------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------
 #   string
-# ----------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------
 
 print("----Ejemplos string", "")
 
@@ -32,8 +39,8 @@ cad2 = "a"
 cad3 = '''todos'''
 cad4 = """ellos"""
 
-#print(cad1, cad2, cad3 + cad4)
-# hola a todosellos
+print(cad1, cad2, cad3 + cad4)
+# output: hola a todosellos
 
 cad5 = ""       # string vacío, tamaño 0
 print(cad5)     # no provoca error, se imprime como linea vacía
@@ -42,15 +49,15 @@ cad6 = None     # NoneType object
 print(cad6)     # no provoca error
 # output: None
 
-# ----------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------
 #   números
-# ----------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------
 
 print("----Ejemplos números", "")
 
 # como en python no hace falta especificar el tipo de dato numerico
 # una forma de especificar una variable como bool es asignar uno de los valores posibles
-# posteriormente se puede modificar
+# y posteriormente se puede modificar
 b = True
 
 # en python los booleanos (bool) son una subclase de los enteros
@@ -69,8 +76,9 @@ d = 29
 e = isinstance(d, (str, int, float, set, list, dict, tuple))
 print("d:", d)
 
-# ----------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------
 #   lista
+# ---------------------------------------------------------------------------------------------
 #       características:
 #           Ordenada: 
 #                   Los elementos dentro de ella están indexados y 
@@ -89,21 +97,38 @@ print("d:", d)
 #       pueden contener cualquier tipo de dato
 #                   enteros, cadenas, booleanos, objetos, listas anidadas, etc 
 #          
-# ----------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------
 
 print("----Ejemplos listas", "")
 
 lista1 = ["0", "2", "9", "14", "20"]
+
+# append() añade elemento
+varl1 = "15"
+lista1.append(varl1)
 print(f"lista1: {lista1}")
 
-var="9"
-print("indice de la variable en lista1:", lista1.index(var))
+# index() recupera el índice
+varl2="9"
+print("indice de la variable en lista1:", lista1.index(varl2))
     # recordar que su indice comienza en 0     
 
+# remove() elimina un determinado elemento 
+lista1.remove(varl2)
+print(f"lista1: {lista1}")
 
-# ----------------------------------------------------------------------------------------
+# sort() orden ascendente
+lista1.sort()
+print(f"lista1 CUIDADO mal ordenada: {lista1}")
+    # CUIDADO no es el resultado que se espera porque es una lista de cadenas
+
+lista2 = [20, 14, 9, 2, 0]
+lista2.sort()
+print(f"lista2 ordenada: {lista2}")
+
+# ---------------------------------------------------------------------------------------------
 #   diccionario
-# ----------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------
 
 print("----Ejemplos diccionarios", "")
 
