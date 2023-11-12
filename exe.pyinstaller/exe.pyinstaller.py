fc2e706365d0a381fbe0aa07b7d56c27c9de55b1
David Cano <davidcanoperez5504@gmail.com>
2023-11-12 02:04:02 +0100

    updated

---
+++ exe.pyinstaller/exe.pyinstaller.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+    pyinstaller
+    
+    web: https://pyinstaller.org/en/stable/
+    
+    PyInstaller bundles a Python application and all its dependencies into a single package. 
+    The user can run the packaged app without installing a Python interpreter or any modules.
+    
+    instalar pyinstaller
+        pip install -U pyinstaller
+        
+    si se ha instalado pyinstaller en el pip de anaconda
+        desplazarse con la terminal prompt al directorio del .py
+        cd C:\ Users\ ususario\Documents\Python Scripts\...
+    
+    comando para empaquetar o compilar
+        pyinstaller your_program.py
+    
+    
+"""
+
